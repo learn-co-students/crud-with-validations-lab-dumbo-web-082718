33444aedb5248e17a6d18ae16c32a562154ef4ae
github username <github email address>
2019-04-13 17:03:25 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,11 @@
 class Song < ActiveRecord::Base
        validates :title, presence: true, allow_blank: false
        # Cannot be repeated by the same artist in the same year ???
-       validates :release_year, presence: true
+       validates :release_year, presence: true, if :release?
+       # validates :released, presence: true, if :release?
 
+  def release?
+    validates :released, presence: true
+  end
 
 end
